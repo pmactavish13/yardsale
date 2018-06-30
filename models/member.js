0501c9aa25fe42784dad17ddf9b29548ce62390d
Randy Reitz <rsreitz@outlook.com>
2018-06-30 10:03:01 -0400

    Category

--- models/member.js
+++ models/member.js
@@ -5,7 +5,7 @@ const memberSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
-  seller: {type: Boolean, default: false },
+  password: {type: Boolean, default: false },
   date: { type: Date, default: Date.now }
 });
 

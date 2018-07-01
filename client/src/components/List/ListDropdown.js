9537867036bb833ac7e6e51c943747022fcc6777
Randy Reitz <rsreitz@outlook.com>
2018-07-01 16:29:59 -0400

    Added some styling for the login form.

--- client/src/components/List/ListDropdown.js
+++ client/src/components/List/ListDropdown.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const ListDropdown = ({ category, children }) => (
-        <li className="nav-item dropdown">
+        <li className="nav-item dropdown pull-right">
         <a className="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           {category}
         </a>

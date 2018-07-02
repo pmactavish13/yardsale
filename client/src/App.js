ed7de546a88db827a9f5408f984c26ff57c3ff52
Randy Reitz <rsreitz@outlook.com>
2018-07-02 01:20:39 -0400

    page set-up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-// import { MemberProfile, MemberSignUp }  from "./pages/Member";
 import Products from "./pages/Products";
+import NewProduct from "./pages/NewProduct";
+import MemberProfile from "./pages/MemberProfile";
+import MemberSignUp from "./pages/MemberSignUp";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Manager, Reference, Popper } from 'react-popper';
 
 const App = () => (
   <Router>
     <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/prodicts/:id" component={Products} />
-        {/* <Route exact path="/member/memberSignUp" component={MemberSignUp} />
-        <Route exact path="/member/memberProfile" component={MemberProfile} /> */}
-        <Route component={NoMatch} />
-      </Switch>
+      <Navigation />
+      <Wrapper>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/product/:id" component={Products} />
+          <Route exact path="/newProduct" component={NewProduct} />
+          <Route exact path="/memberSignUp" component={MemberSignUp} />
+          <Route exact path="/member/:id" component={MemberProfile} />
+          <Route component={NoMatch} />
+        </ Switch>
+      </Wrapper>
+      <Footer />
     </div>
   </Router>
 );
--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,15 +2,11 @@ import React from "react";
 import "./Jumbotron.css";
 
 const Jumbotron = ({ children }) => (
-  <div className="jumbotron jumbotron-fluid">
-  <div className="container">
-    <h1 className="display-4">Welcome to Yardsale!</h1>
-    <p className="lead">We list items for sale locally.</p>
-    <p className="lead">Provide you with a messaging system to contact the buyer/seller.</p>
-    <p className="lead">And a payment system to complete the transaction.</p>
+  <div className="frame">
+    <div className="jumbotron">
+      {children}
+    </div>
   </div>
-  {children}
-</div>
 );
 
 export default Jumbotron;

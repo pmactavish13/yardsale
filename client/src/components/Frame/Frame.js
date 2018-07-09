44626e6d52d226d16a7bb310b2e303b8b25978a4
Randy Reitz <rsreitz@outlook.com>
2018-07-08 22:15:44 -0400

    save images to db

--- client/src/components/Frame/Frame.js
+++ client/src/components/Frame/Frame.js
@@ -2,9 +2,7 @@ import React from "react";
 import "./Frame.css";
 
 const Frame = props => {
-  console.log(props)
   return (<div className="frame">
-
     {props.children}
   </div>)
 }
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -24,7 +24,7 @@ export default class Navigation extends React.Component {
       password: "",
       isOpen: false,
       //*** fake Authorization ******/
-      isLoggedIn: true
+      isLoggedIn: false
       //*****************************/
     };
 

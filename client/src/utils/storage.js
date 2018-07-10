f370bcdfeb35fa6386891f27cf5720088a4db8c1
Randy Reitz <rsreitz@outlook.com>
2018-07-09 20:34:03 -0400

    tokens now in storage.

--- client/src/utils/storage.js
+++ client/src/utils/storage.js
@@ -1,29 +1,36 @@
-export function getFromStorage(key) {
-    if (!key) {
-        return null;
-    }
+export default {
 
-    try {
-        const valueStr = localStorage.getItem(key);
-        if (valueStr) {
-
-            return JSON.parse(valueStr);
+    //*************** STORAGE *************************/
+    // Gets key from storage
+    getFromStorage: function (key) {
+        if (!key) {
+            return null;
         }
-        return null;
-    
-    } catch (err) {
-        return null;
-    }
-}
 
-export function setInStorage(key, obj) {
-    if (!key) {
-        console.log('Error: Key is missing');
+        try {
+            const valueStr = localStorage.getItem(key);
+            if (valueStr) {
+
+                return JSON.parse(valueStr);
+            }
+            return null;
+
+        } catch (err) {
+            return null;
+        }
+    },
+
+    // Sets key in storage
+    setInStorage: function (key, obj) {
+        if (!key) {
+            console.log('Error: Key is missing');
+        }
+        try {
+            localStorage.setItem(key, JSON.stringify(obj));
+
+        } catch (err) {
+            console.log(err);
+        }
     }
-    try {
-        localStorage.setItem(key, JSON.stringify(obj));
-    
-    } catch (err) {
-        console.log(err);
-    }
-}
+
+};

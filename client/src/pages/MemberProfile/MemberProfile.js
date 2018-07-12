26319df3128d7fb90e9c1e50e8143fd76044d759
Paula MacTavish <perilousp@hotmail.com>
2018-07-12 17:55:59 -0400

    login modal

--- client/src/pages/MemberProfile/MemberProfile.js
+++ client/src/pages/MemberProfile/MemberProfile.js
@@ -4,27 +4,37 @@ import API from "../../utils/API";
 // import { Link } from "react-router-dom";
 import { Row, Column } from "../../components/Grid";
 import { FormContainer } from "../../components/Form";
-// import Frame from "../../components/Frame";
+import Frame from "../../components/Frame";
+// import ProductCard from "../../components/ProductCard";
 import "./MemberProfile.css";
 
 class MemberProfile extends Component {
-    // ({match: {params{id}}})
+    constructor(props) {
+        super(props);
 
-    // Setting the initial values of ex: this.state.username
-    state = {
-        email: "",
-        username: "",
-        password: "",
-        firstName: "",
-        lastName: "",
-        phoneNum: ""
-    };
-
-    loadNewMembers = () => {
-        //alert(`You are signed up!\nemail: ${this.state.email}\nUsername: ${this.state.username}\nPassword: ${this.state.password}\nFirst Name: ${this.state.firstName}\nLast Name: ${this.state.lastName}\nPhone Number: ${this.state.phoneNum}`)
-        this.setState({ email: "", username: "", password: "", firstName: "", lastName: "", phoneNum: "" })
-        //window.location.href = '/products';
+        // Setting the initial values of ex: this.state.username
+        this.state = {
+            member: {}
+            // email: "",
+            // username: "",
+            // password: "",
+            // firstName: "",
+            // lastName: "",
+            // phoneNum: ""
+        };
     }
+    componentDidMount() {
+        // this.loadMember();
+        API.getMember(this.props.match.params.id)
+            .then(res => this.setState({ member: res.data }))
+            .catch(err => console.log(err))
+    }
+
+    // loadNewMembers = () => {
+    //     //alert(`You are signed up!\nemail: ${this.state.email}\nUsername: ${this.state.username}\nPassword: ${this.state.password}\nFirst Name: ${this.state.firstName}\nLast Name: ${this.state.lastName}\nPhone Number: ${this.state.phoneNum}`)
+    //     this.setState({ email: "", username: "", password: "", firstName: "", lastName: "", phoneNum: "" })
+    //     //window.location.href = '/products';
+    // }
 
     // handle any changes to the input fields
     handleNewMemberInputChange = event => {
@@ -71,102 +81,127 @@ class MemberProfile extends Component {
 
     render() {
         return (
-            // <Frame>
-            <FormContainer>
-                <h3>Member Enrollment Form</h3>
-                <form>
-                    <div className="formgroup">
-                        <Row>
-                            <Column size="md-12">
-                                <label>Email</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    className="form-control form-control-sm"
-                                    placeholder="email@profile.com"
-                                    value={this.state.email}
-                                    onChange={this.handleNewMemberInputChange}
-                                />
-                            </ Column>
-                        </Row>
-                    </div>
+            <Frame>
+                <FormContainer>
+                    <h3>Member Enrollment Form</h3>
+                    <form>
+                        <div className="formgroup">
+                            <Row>
+                                <Column size="md-12">
+                                    <label>Email</label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        className="form-control form-control-sm"
+                                        placeholder="email@profile.com"
+                                        value={this.state.email}
+                                        onChange={this.handleNewMemberInputChange}
+                                    />
+                                </ Column>
+                            </Row>
+                        </div>
 
-                    <div className="formgroup">
-                        <Row>
-                            <Column size="md-6">
-                                <label>User Name</label>
-                                <input
-                                    type="text"
-                                    name="username"
-                                    className="form-control form-control-sm"
-                                    placeholder="Posted with any Listing"
-                                    value={this.state.username}
-                                    onChange={this.handleNewMemberInputChange} />
-                            </ Column>
-                            <Column size="md-6">
-                                <label>Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    className="form-control form-control-sm"
-                                    placeholder="Password Placeholder"
-                                    value={this.state.password}
-                                    onChange={this.handleNewMemberInputChange} />
-                            </ Column>
-                        </Row>
-                    </div>
+                        <div className="formgroup">
+                            <Row>
+                                <Column size="md-6">
+                                    <label>User Name</label>
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        className="form-control form-control-sm"
+                                        placeholder="Posted with any Listing"
+                                        value={this.state.username}
+                                        onChange={this.handleNewMemberInputChange} />
+                                </ Column>
+                                <Column size="md-6">
+                                    <label>Password</label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        className="form-control form-control-sm"
+                                        placeholder="Password Placeholder"
+                                        value={this.state.password}
+                                        onChange={this.handleNewMemberInputChange} />
+                                </ Column>
+                            </Row>
+                        </div>
 
-                    <div className="formgroup">
-                        <Row>
-                            <Column size="md-12">
-                                <label>First Name</label>
-                                <input
-                                    type="text"
-                                    name="firstName"
-                                    className="form-control form-control-sm"
-                                    placeholder="First Name Placeholder"
-                                    value={this.state.firstName}
-                                    onChange={this.handleNewMemberInputChange} />
-                            </ Column>
-                        </Row>
-                    </div>
+                        <div className="formgroup">
+                            <Row>
+                                <Column size="md-12">
+                                    <label>First Name</label>
+                                    <input
+                                        type="text"
+                                        name="firstName"
+                                        className="form-control form-control-sm"
+                                        placeholder="First Name Placeholder"
+                                        value={this.state.firstName}
+                                        onChange={this.handleNewMemberInputChange} />
+                                </ Column>
+                            </Row>
+                        </div>
 
-                    <div className="formgroup">
-                        <Row>
-                            <Column size="md-12">
-                                <label>Last Name</label>
-                                <input
-                                    type="text"
-                                    name="lastName"
-                                    className="form-control form-control-sm"
-                                    placeholder="Last Name Placeholder"
-                                    value={this.state.lastName}
-                                    onChange={this.handleNewMemberInputChange} />
-                            </ Column>
-                        </Row>
-                    </div>
+                        <div className="formgroup">
+                            <Row>
+                                <Column size="md-12">
+                                    <label>Last Name</label>
+                                    <input
+                                        type="text"
+                                        name="lastName"
+                                        className="form-control form-control-sm"
+                                        placeholder="Last Name Placeholder"
+                                        value={this.state.lastName}
+                                        onChange={this.handleNewMemberInputChange} />
+                                </ Column>
+                            </Row>
+                        </div>
 
-                    <div className="formgroup">
-                        <Row>
-                            <Column size="md-12">
-                                <label>Phone Number</label>
-                                <input
-                                    type="number"
-                                    name="phoneNum"
-                                    className="form-control form-control-sm"
-                                    placeholder="Phone Number Placeholder"
-                                    value={this.state.phoneNum}
-                                    onChange={this.handleNewMemberInputChange} />
-                            </ Column>
-                        </Row>
-                    </div>
-                    <button type="submit" className="btn btn-primary newMember" onClick={this.handleNewMemberFormSubmit}>SIGN UP</button>
-                </form>
-            </FormContainer>
-            // </Frame>
-        );
-    }
-}
+                        <div className="formgroup">
+                            <Row>
+                                <Column size="md-12">
+                                    <label>Phone Number</label>
+                                    <input
+                                        type="number"
+                                        name="phoneNum"
+                                        className="form-control form-control-sm"
+                                        placeholder="Phone Number Placeholder"
+                                        value={this.state.phoneNum}
+                                        onChange={this.handleNewMemberInputChange} />
+                                </ Column>
+                            </Row>
+                        </div>
+                        <button type="submit" className="btn btn-primary newMember" onClick={this.handleNewMemberFormSubmit}>SIGN UP</button>
+                    </form>
+                </FormContainer>
 
-export default MemberProfile;
+                {/* <Row>
+                    {this.state.products.map(product => ( 
+                    <Column size="md-6" key={this.state.product._id}> */}
+                        
+                        {/* <ProductCard key={this.state.product._id}>
+                            <div className="img-container">
+                                <img className="productImage" alt={this.state.product.item} src={this.state.product.image1} />
+                            </div>
+                            <div className="content">
+                                <ul>
+                                    <li>
+                                        <strong>Item:</strong> {this.state.product.item}
+                                    </li>
+                                    <li>
+                                        <strong>Description:</strong> {this.state.product.description}
+                                    </li>
+                                    <li>
+                                        <strong>Price: $</strong> {this.state.product.price}
+                                    </li>
+                                </ul>
+                            </div>
+                        </ProductCard> */}
 
+                    {/* </Column> */}
+             </Frame>
+                );
+            }
+        }
+        
+        export default MemberProfile;
+        

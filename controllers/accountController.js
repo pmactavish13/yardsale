2721e90077873f854c00845a431cf51e91d01103
Randy Reitz <rreitz@4rsystems.com>
2018-07-13 19:50:52 -0400

    Signout removes session from database.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -78,39 +78,33 @@ module.exports = {
     signOut: function (req, res) {
         const { body } = req;
         const { token } = body;
+
         if (!token) {
             return res.send({
                 success: false,
                 message: 'Error: Token cannot be blank.'
             })
         };
-        // TODO:  Actually delete the token
-        return res.send({
-            success: true,
-            message: 'Valid sign Out',
-            token: ''
+        // TODO: Revisit setting state to deleted
+        db.Session.remove({
+            _id: token,
+            isDeleted: false
+        }, (err) => {
+            if (!err) {
+                return res.send({
+                    success: false,
+                    message: 'Server Error',
+                    err: err
+                });
+            } else {
+                return res.send({
+                    success: true,
+                    message: 'Valid sign Out',
+                    token: ''
+                });
+            }
         });
-        // //otherwise launch a session
-        // const session = new db.Session();
-        // session.userId = member._id;
-        // session.remove((err, doc) => {
-        //     if (err) {
-        //         return res.send({
-        //             success: false,
-        //             message: 'Error: Server Error 154.',
-        //             err: err
-        //         });
-        //     }
-
-        //     return res.send({
-        //         success: true,
-        //         message: 'Valid sign Out',
-        //         token: ''
-        //     });
-
-        // });
-    }
-    ,
+    },
 
     verify: function (req, res) {
         const { body } = req;
@@ -122,95 +116,5 @@ module.exports = {
             message: 'User Logged in'
         });
     }
-    // app.post('/account/signup', (req, res, next) => {
-    //     const { body } = req;
-    //     const { firstName, lastName, password } = body;
-    //     let { email } = body;
 
-
-    //     if (!firstName) {
-    //         return res.send({
-    //             success: false,
-    //             message: 'Error: first name cannot be blank.'
-    //         })
-    //     }
-
-    //     if (!lastName) {
-    //         return res.send({
-    //             success: false,
-    //             message: 'Error: last name cannot be blank.'
-    //         })
-    //     }
-
-    //     if (!password) {
-    //         return res.send({
-    //             success: false,
-    //             message: 'Error: password cannot be blank.'
-    //         })
-    //     }
-
-    //     if (!email) {
-    //         return res.send({
-    //             success: false,
-    //             message: 'Error: email cannot be blank.'
-    //         })
-    //     }
-
-    //     email = email.toLowerCase();
-
-    //     // steps:
-    //     // 1) Verify email doesn't exist
-    //     // 2) Save it
-    //     Member.find({
-    //         email: email
-    //     }, (err, previousMembers) => {
-    //         if (err) {
-    //             return res.send({
-    //                 success: false,
-    //                 message: 'Error: Server Error 66.'
-    //             });
-    //         } else if (previousMembers.length > 0) {
-    //             return res.send({
-    //                 success: false,
-    //                 message: 'Error: Unable to complete Signup.'
-    //             });
-    //         }
-
-        // // TODO:  Verify token is marked deleted in DB
-        // return res.send({
-        //     success: true,
-        //     message: 'Valid sign Out',
-        //     token: ''
-        // });
-
-    // },
-    // verify: function (req, res) {
-    //     const { body } = req;
-    //     const { token } = body;
-
-    //     // TODO:  Actually Verify the user session againts the DB...
-
-    //     db.Session.find({
-    //         _id: token,
-    //         isDeleted: false
-    //     }, (err, sessions) => {
-    //         console.log(sessions[0].userId);
-    //         if (err) {
-    //             return res.send({
-    //                 success: false,
-    //                 message: 'Error: Server Error 130.'
-    //             });
-    //         } else if (sessions.length != 1) {
-    //             return res.send({
-    //                 success: false,
-    //                 message: 'Session Error'
-    //             });
-    //         }
-
-    //         return res.send({
-    //             success: true,
-    //             message: 'User Logged in'
-    //         });
-    //     });
-    // }
 };

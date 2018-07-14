44dccd7841a499efdb3f64e1d6d81e148955a62f
Randy Reitz <rsreitz@outlook.com>
2018-07-13 23:42:25 -0400

    connect models api calls

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Navigation />
+          <Navigation/>
           <Wrapper>
             <Switch>
               <Route exact path="/" component={Home} />

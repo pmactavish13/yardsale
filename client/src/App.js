6371452e362e441b6a6ee65845886fb28ee61259
Randy Reitz <rreitz@4rsystems.com>
2018-07-17 19:51:43 -0400

    Added Auth0 navigation to page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SafetyTips from "./pages/SafetyTips";
@@ -14,6 +15,20 @@ import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 // import 'bootstrap/dist/css/bootstrap.min.css';
+// Auth0 Adds ...
+import Callback from './Callback/Callback';
+import Auth from './Auth/Auth';
+import history from './history';
+
+const auth = new Auth();
+
+const handleAuthentication = ({ location }) => {
+  console.log(location);
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+// ... Auth0 Adds
 
 class App extends Component {
   constructor(props) {
@@ -28,9 +43,14 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      // <Router>
+      <Router history={history}>
         <div>
-          <Navigation/>
+          <Navigation auth={auth} {...this.props} />
+          <Route path="/callback" render={(newProps) => {
+                handleAuthentication(newProps);
+                return <Callback {...this.props} />
+              }} />
           <Wrapper>
             <Switch>
               <Route exact path="/" component={Home} />

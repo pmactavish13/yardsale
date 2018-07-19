a7a08e5356dd2425c87280f6817090f884b6e7f6
Paula MacTavish <perilousp@hotmail.com>
2018-07-19 01:54:02 -0400

    notes

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,30 +1,30 @@
 const db = require("../models");
-const mongoose = require('mongoose');
 
 // Defining methods for the notesController
 module.exports = {
-find: function(req, res) {
-  console.log("notesController" + req.params.id)
+  find: function (req, res) {
+    console.log("body " + req.params.ids);
     db.Note
-      .find({product_id:req.params.id})
+    .find (req.params.ids)
+      // .find({ product_id: req.params.id, member_id: req.body.member._id })
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: function (req, res) {
     console.log("notesController create" + req.body)
     db.Note
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: function (req, res) {
     db.Note
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function(req, res) {
+  remove: function (req, res) {
     db.Note
       .find({ product_id: req.params.id })
       .then(dbModel => dbModel.remove())

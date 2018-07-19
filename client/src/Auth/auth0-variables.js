41018d065d04f268f50655bc72127d6567456dfb
Randy Reitz <rsreitz@outlook.com>
2018-07-19 14:43:02 -0400

    pointing callback for heroku deployment.

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'yrdsale.auth0.com',
   clientId: 'OpgtTZHUetAiAQe2jpat5Cs3tL7AU8t7',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl:  'https://whispering-plateau-37833.herokuapp.com/callback'
+  // callbackUrl:  'http://localhost:3000/callback'
 }
